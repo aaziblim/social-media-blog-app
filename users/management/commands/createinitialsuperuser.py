1748789b13c7c2411ffae9af7ff1d34d6441f867
aaziblim <azizjibril559@gmail.com>
2025-05-06 17:08:39 +0000

    made changes to the Dockerfile

--- users/management/commands/createinitialsuperuser.py
+++ users/management/commands/createinitialsuperuser.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(username='azitech').exists():
+        if not User.objects.filter(username='aziztech').exists():
             superuser_password = os.getenv('SUPERUSER_PASSWORD', 'default_password')  # Default fallback
-            User.objects.create_superuser('azitech', 'azizmelzer@gmail.com', superuser_password)
+            User.objects.create_superuser('aziztech', 'azizmelzer@gmail.com', superuser_password)
             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
         else:
             self.stdout.write(self.style.WARNING('Superuser already exists'))
